57d94c03be23eb44b120cb7713425408995e330d
LUCCA SENA <lucca.sena@cs.unipe.edu.br>
2025-02-21 16:22:33 -0300

    segundo-dia

--- dia2/dia2.sql
+++ dia2/dia2.sql
@@ -1,2 +1,24 @@
+-- Fábrica de Softare 2025.01 - Dia 2
 
-SELECT * FROM membros;
+SELECT * 
+FROM membros;
+
+-- 1: Seleciona todas as variáveis da tabela membros que retorna todos os registros com RenewStatus = On de maneira crescente.
+SELECT * 
+FROM membros 
+WHERE RenewStatus = 'On' 
+ORDER BY PersonID ASC;
+
+-- 2: Inserido um registro na tabela membros:
+INSERT INTO 
+membros(YearMonth, PersonID, FirstName, LastName) 
+VALUES ('jan/25', 1111111, 'Gary', 'Rainer');
+
+-- 3: Atualizando um dado de um registro na tabela membros:
+UPDATE  membros
+SET Email = 'garyrainer@gmail.com'
+WHERE PersonID = 1111111;
+
+-- 4: Deletando um registro na tabela:
+DELETE FROM  membros
+WHERE PersonID = 1111111;
